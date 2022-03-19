49b010b797520350ce208e03f83fcab997470545
MICHAELU-PC\yzx-3 <yzx-363@163.com>
2022-03-19 19:41:46 +0800

    initial: add projects.

---
+++ 疫情数据统计/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using statistic;
+
+Console.WriteLine("Hello, World!");
+
+Analyser analyser = new Analyser();
+analyser.Analyse();
+
+//Console.WriteLine($"新增4例新冠肺炎本土确诊病例和101例本土无症状感染者 to {"新增4例新冠肺炎本土确诊病例和101例本土无症状感染者".String2Unicode()}");
+//Console.WriteLine($"4 to {"4".String2Unicode()}");
+//Console.WriteLine($"101 to {"101".String2Unicode()}");
+Console.WriteLine("done");
+Console.ReadKey();
+
